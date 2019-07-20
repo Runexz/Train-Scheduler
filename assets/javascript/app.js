6a53117c0f81f716c7a36713ae906bd66263826d
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-07-19 21:15:49 -0500

    created on click for button submit js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
 
 
 console.log("hello world");
+
+var trainStatCount = 0;
+
+// when the submit button is clicked this function will run
+$("#addInfo").on("click", function(event) {
+    event.preventDefault();
+
+    // get the train name from input and store it into a variable
+    var trainNameEntry = $("#addTrainName").val().trim();
+
+
+})
