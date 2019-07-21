a74d66cd13ef61c47ae944a968eed89bf87308e8
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-07-21 10:09:13 -0500

    included firebase functions to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@
 console.log("hello world");
 
 
-// Your web app's Firebase configuration
+// my web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyBeaeeRZRhQxOMbKmwcBok28_H9Meolyp8",
     authDomain: "train-scheduler-87b65.firebaseapp.com",
@@ -16,6 +16,9 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// create a variable to reference the database
+var database = firebase.database();
+
 
 var trainStatCount = 0;
 
@@ -23,8 +26,30 @@ var trainStatCount = 0;
 $("#addInfo").on("click", function (event) {
     event.preventDefault();
 
-    // get the train name from input and store it into a variable
+    // get the train info from input and store it into a variable
     var trainNameEntry = $("#addTrainName").val().trim();
+    var trainDestinationEntry = $("#addDestination").val().trim();
+    var trainTimeEntry = parseInt($("#addTrainTime").val().trim());
+    var trainFrequencyEntry = parseInt($("#addFrequency").val().trim());
 
+    // save the new input entries in Firebase.  
+    database.ref().set({
+        trainName: trainNameEntry,
+        trainDestination: trainDestinationEntry,
+        trainTime: trainTimeEntry,
+        trainFrequency: trainFrequencyEntry
+    });
+    console.log("Train name " + data.trainName)
 
-})
+});
+
+// at initial load and subsequent value chages, get a snapshot of the stored data
+// function to update page in real-time when the firebase database changes
+database.ref().on("value", function(snapshot) {
+
+    // if any error are experiencced log them to console
+    //  function(errorObject) {
+    //     console.log("the read failed: " + errorObject.code)
+    // }
+
+});
